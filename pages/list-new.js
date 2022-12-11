43c5d01759cc893f0042b915b5af28f1e1d9727f
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-12-10 21:14:49 -0500

    added character limits to text fields and installed pg

--- pages/list-new.js
+++ pages/list-new.js
@@ -12,19 +12,19 @@ export default function ListNew() {
             <card>
             <form action="/send-data-here" method="post" className={styles.form}>
                 <label for="itemTitle"  className={styles.label}>Title</label>
-                <input type="text" id="itemTitle" name="itemTitle" className={styles.textInput}/>
+                <input type="text" id="itemTitle" name="itemTitle" maxLength={255} className={styles.textInput}/>
 
                 <label for="itemImage"className={styles.label}>Upload Image</label>
                 <img src="/UploadIcon.png" className={styles.img}/>
 
                 <label for="itemCost"className={styles.label}>Cost</label>
-                <input type="text" id="itemCost" name="itemCost" className={styles.textInput}/>
+                <input type="text" id="itemCost" name="itemCost" maxLength={31} className={styles.textInput}/>
 
                 <label for="itemDescription"className={styles.label}>Description</label>
-                <textarea type="text" id="itemDescription" name="itemDescription" className={styles.textInput}/>
+                <textarea type="text" id="itemDescription" name="itemDescription" maxLength={500} className={styles.textInput}/>
 
                 <label for="itemContact"className={styles.label}>Name and Contact Info</label>
-                <input type="text" id="itemContact" name="itemContact" className={styles.textInput}/>
+                <input type="text" id="itemContact" name="itemContact" maxLength={255} className={styles.textInput}/>
 
                 <button type="submit" className={styles.button}>Submit</button>
             </form>
